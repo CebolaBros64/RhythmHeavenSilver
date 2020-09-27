437373a93abfb95783cd2ae4ac6abc5266856831
CebolaBros64 <vimniciusmaster@gmail.com>
2020-09-27 15:22:01 -0300

    Start work on Portuguese translation

---
+++ PT-BR_SourceCode/Graphics/Rhythm_Games/Rhythm_Tweezers/Veggie1.asm
@@ -0,0 +1,14 @@
+.org 0x08BEE62C
+.dw bee62c_veggie1_intro |0x80000000
+//Title
+.org 0x088AF6B4
+.dw XD45D4E_assembly0
+.org 0x088AF6BC
+.dw XD45D68_assembly1
+.org 0x088AF6C4
+.dw XD45D82_assembly2
+.org 0x088AF6CC
+.dw XD45D9C_assembly3
+.org 0x088AF6D4
+.dw XD45DB6_assembly4
+;eof
